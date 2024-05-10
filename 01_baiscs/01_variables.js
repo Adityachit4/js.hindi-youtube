bc4a55a1a76a009fed4343b078985d4d206595fe
Aditya Chitransh <124768879+Adityachit4@users.noreply.github.com>
2024-05-10 07:29:22 +0000

    variables material

---
+++ 01_baiscs/01_variables.js
@@ -0,0 +1,19 @@
+const accountid =23456
+let accountemail = "adityachit4@gmail"
+var accountpassword = "135456"
+accountcity ="mumbai"
+let accountstate
+
+
+// accountid =2   // not allowed
+accountemail ="aaddi@gmail"
+accountpassword ="266447"
+accountcity ="ghazipur"
+
+// console.log(accountid)
+console.log(accountemail)
+console.log(accountpassword)
+console.log(accountcity)
+
+// or new way to print all in one table 
+console.table([accountid, accountemail,accountpassword,accountcity,accountstate])
